c5ee7565bda859c7d2f8cdbdc0f07daa8abb3fa2
DyshkantAleks <o.dushkant@gmail.com>
2020-10-16 14:57:19 +0300

    first commit

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://admin:7Fu6MwivyNNVYMd@cluster0.tmz9f.mongodb.net/app?retryWrites=true&w=majority',
+  secretOrKey: 'random very very secret string',  
+};
